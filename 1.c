0484962961060ae7d64559a8b75268c65f80616e
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-01-08 17:00:39 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a=20;
+printf("%d",a);
+if(a>0)
+{
+printf("%d is positive",a);
+}
+else if(a<o)
+{
+printf("%d is negative",a):
+}
+else
+{
+printf("%d is 0);
+}
+return 0;
+}
